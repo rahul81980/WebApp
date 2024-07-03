3b99a07b93bf74ad59745b9e8bfd70436b82b8b2
rahul81980 <rahulsharma81980@gmail.com>
2024-07-02 17:24:37 -0700

    change today by rahul

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGODB).then(()=>{
     console.log("connected successfully");
 }).catch((err)=>{
-    console.log("An error occur",err);
+    console.log("An error occur",err.message);
 })
 const db = mongoose.connection
 db.on("open",()=>console.log("connection open"))
 
-db.on("error",(err)=>console.log("an error occur",err))
+db.on("error",(err)=>console.log("an error occur",err.message))
 
 module.exports = db
